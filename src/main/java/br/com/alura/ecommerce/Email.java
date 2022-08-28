d84e0b3db7812409f2499d509723ceb018ae9afb
Marcos Araujo <marcos.txt@gmail.com>
2022-08-28 20:05:11 -0300

    first commit

---
+++ src/main/java/br/com/alura/ecommerce/Email.java
@@ -0,0 +1,12 @@
+package br.com.alura.ecommerce;
+
+public class Email {
+
+    private final String object, body;
+
+    public Email(String object, String body) {
+        this.object = object;
+        this.body = body;
+    }
+
+}
---
+++ src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -0,0 +1,46 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class EmailService {
+
+    public static void main(String[] args) {
+        var emailService = new EmailService();
+        try(var service = new KafkaService(EmailService.class.getSimpleName(),
+                "ECOMMERCE_SEND_EMAIL", emailService::parse, String.class)) {
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("-----");
+        System.out.println("Send email");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Email sent");
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
+        return properties;
+    }
+
+}
